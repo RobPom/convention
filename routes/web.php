8a143837a436a4c433bca08295f5768cf055caba
Rob Pomerleau <pomerleau.robert@gmail.com>
2018-05-22 20:37:01 -0600

    set up env file, initialize auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/', 'HomeController@welcome')->name('welcome');
+
+Route::get('/home', 'HomeController@index')->name('home');
