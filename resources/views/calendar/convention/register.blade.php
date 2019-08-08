65b7eb414664f08e84ba77c2bc4a34dfc4094e94
Rob Pomerleau <pomerleau.robert@gmail.com>
2019-08-08 14:56:32 -0600

    added paypal info

--- resources/views/calendar/convention/register.blade.php
+++ resources/views/calendar/convention/register.blade.php
@@ -35,12 +35,12 @@
                
                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                     <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="8KAFWF8UH2F38">
+                    <input type="hidden" name="hosted_button_id" value="TWPPJK5JM3L2C">
                     <table>
                     <tr><td><input type="hidden" name="on0" value="Number of Attendees">Number of Attendees</td></tr><tr><td><select name="os0">
-                        <option value="One person">One person $25.00 CAD</option>
-                        <option value="Two people">Two people $50.00 CAD</option>
-                        <option value="Three people">Three people $75.00 CAD</option>
+                        <option value="One person">One person $36.00 CAD</option>
+                        <option value="Two people">Two people $72.00 CAD</option>
+                        <option value="Three people">Three people $108.00 CAD</option>
                     </select> </td></tr>
                     </table>
                     <input type="hidden" name="currency_code" value="CAD">
