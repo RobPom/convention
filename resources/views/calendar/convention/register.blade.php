28b497541a476ef5d54abcb6c685f60673f1ff9d
Rob Pomerleau <pomerleau.robert@gmail.com>
2021-08-20 17:27:24 -0600

    troubleshoot paypal error

--- resources/views/calendar/convention/register.blade.php
+++ resources/views/calendar/convention/register.blade.php
@@ -31,19 +31,19 @@
                 <small><em>After payment is complete, click 'Return to Merchant' to come back to this site, and start picking games!</em></small>
              </div>
              <div class="col-sm-6 text-center text-sm-left">
-               <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                    <input type="hidden" name="cmd" value="_s-xclick">
-                    <input type="hidden" name="hosted_button_id" value="MNE4SBZM7T95U"
-                    <table>
-                      <tr><td><input type="hidden" name="on0" value="Number of Attendees">Number of Attendees </td></tr><tr><td><select name="os0">
-                          <option value="One person">One person $35.00 CAD</option>
-                          <option value="Two people">Two people $70.00 CAD</option>
-                          <option value="Three people">Three people $105.00 CAD</option>
-                      </select> </td></tr>
-                    </table>
-                    <input type="hidden" name="currency_code" value="CAD">
-                    <input class='mt-2' type="image" src="/img/paypal.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="MNE4SBZM7T95U">
+                <table>
+                <tr><td><input type="hidden" name="on0" value="Number of Attendees">Number of Attendees</td></tr><tr><td><select name="os0">
+                <option value="One">One $35.00 CAD</option>
+                <option value="Two">Two $70.00 CAD</option>
+                <option value="Three">Three $105.00 CAD</option>
+                </select> </td></tr>
+                </table>
+                <input type="hidden" name="currency_code" value="CAD">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
                 </form>
              </div>
          </div>
