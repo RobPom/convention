ff64018aa7b7cd67209ee79c6126481773570666
Rob Pomerleau <pomerleau.robert@gmail.com>
2023-03-24 17:29:15 -0600

    fixed landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,13 +4,14 @@
 
 @if ( App\Convention::where('status' , 'active')->count() == 1)
 
-  @include('calendar.convention.banners.fall2022.default')
+      @include('calendar.convention.banners.spring2023.default')
 
-@else
+    @else
 
-  @include('calendar.convention.banners.default')
+      @include('calendar.convention.banners.default')
 
 @endif
+
 <div class="row">
     <div class="col-md-12">
        <div class="lead my-1"> Welcome to the home of IntrigueCon, a tabletop roleplaying game convention based in Edmonton, Alberta, Canada. </div>
