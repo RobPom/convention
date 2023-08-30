7df108b9193fe13268986eb86832ddfa6039f3e3
Rob Pomerleau <pomerleau.robert@gmail.com>
2023-08-30 15:30:00 -0600

    gx test fix 07

--- resources/views/calendar/convention/banners/fall2023/default.blade.php
+++ resources/views/calendar/convention/banners/fall2023/default.blade.php
@@ -1,10 +1,4 @@
 <style>
-  .banner-xl {
-    box-shadow: inset 0px 0px 400px rgba(0, 0, 0, 0.25);
-    background: url(/img/fall2023/Fall2023_Banner_400_xl.jpg) no-repeat;
-    background-size: cover;
-    min-height: 400;
-  }
 
   .banner-large {
     box-shadow: inset 0px 0px 400px rgba(0, 0, 0, 0.25);
@@ -15,28 +9,27 @@
 
   .banner-medium {
     box-shadow: inset 0px 0px 400px rgba(0, 0, 0, 0.25);
-    background: url(/img/fall2023/Fall2023_Banner_250.jpg) no-repeat;
+    background: url(/img/fall2023/fall2023_Banner_250.jpg) no-repeat;
     background-size: cover;
     min-height: 250px;
   }
 
   .banner-small {
     box-shadow: inset 0px 0px 400px rgba(0, 0, 0, 0.25);
-    background: url(/img/fall2023/Fall2023_Banner_200.jpg) no-repeat;
+    background: url(/img/fall2023/fall2023_Banner_200.jpg) no-repeat;
     background-size: cover;
     min-height: 200px;
   }
 
   .banner-xs {
-    background:url(/img/fall2023/100/fall2023_Apoc_100.png) no-repeat;
+    background:url(/img/fall2023/fall2023_Apoc_100.png) no-repeat;
     background-size: 50%;
-    background-position: 50% 50%;
+    background-position:50%;
     min-height: 100px;
   }
 </style>
 
-  <!-- xs -->     <div class="d-block d-xs-block d-sm-none d-md-none d-lg-none d-xl-none banner-xs"></div>
-  <!-- small -->  <div class="d-none d-sm-block d-md-none d-lg-none d-xl-none banner-small"></div>
-  <!-- medium --> <div class="d-none d-sm-none d-md-block d-lg-none d-xl-none banner-medium"></div>
-  <!-- large -->  <div class="d-none d-sm-none d-md-none d-lg-block d-xl-none banner-large"></div>
-  <!-- xl -->     <div class="d-none d-sm-none d-md-none d-lg-none d-xl-block banner-xl"></div>
+  <!-- xs -->     <div class="d-block d-sm-none banner-xs"></div>
+  <!-- small -->  <div class="d-none d-sm-block d-md-none banner-small"></div>
+  <!-- medium --> <div class="d-none d-md-block d-lg-none banner-medium"></div>
+  <!-- large -->  <div class="d-none d-lg-block banner-large"></div>
