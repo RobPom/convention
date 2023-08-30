ef46c02e2168af0a69e8f61fb1e685ca2bb282e9
Rob Pomerleau <pomerleau.robert@gmail.com>
2023-08-30 14:56:11 -0600

    gx test fix 01

--- resources/views/calendar/convention/banners/fall2023/default.blade.php
+++ resources/views/calendar/convention/banners/fall2023/default.blade.php
@@ -1,21 +1,15 @@
 <style>
-
   .banner-xl {
     box-shadow: inset 0px 0px 400px rgba(0, 0, 0, 0.25);
-    background:     url(/img/fall2023/400/fall2023_Apoc_400.png) no-repeat, 
-                    url(/img/fall2023/400/fall2023_Logo_400.png) no-repeat, 
-                    url(/img/fall2023/400/fall2023_Background_400.png) no-repeat;
-    background-size: 400, 400, cover;
+    background:url(/img/fall2023/400/fall2023_Backgroun_400.png) no-repeat;
+    background-size: cover;
     min-height: 400px;
   }
 
   .banner-large {
     box-shadow: inset 0px 0px 400px rgba(0, 0, 0, 0.25);
-    background:     url(/img/fall2023/400/fall2023_Apoc_400.png) no-repeat, 
-                    url(/img/fall2023/400/fall2023_Logo_400.png) no-repeat, 
-                    url(/img/fall2023/400/fall2023_Background_400.png) no-repeat;
-    background-size: 400, 400, cover;
-    background-position: 0, 100% 90%, 0;
+    background: url(/img/fall2023/400/fall2023_Background_400.png) no-repeat;
+    background-size: cover;
     min-height: 400px;
   }
 
@@ -39,40 +33,15 @@
   }
 
   .banner-xs {
-
     background:     url(/img/fall2023/100/fall2023_Apoc_100.png) no-repeat;
     background-size: 50%;
     background-position: 50% 50%;
     min-height: 100px;
   }
-
 </style>
 
-  <!-- xs screens -->
-  <div class="d-block d-xs-block d-sm-none d-md-none d-lg-none d-xl-none banner-xs">
-
-  </div>
-  
-  <!-- small screens -->
-  <div class="d-none d-sm-block d-md-none d-lg-none d-xl-none banner-small">
-
-  </div>
-
-   <!-- medium screens -->
-   <div class="d-none d-sm-none d-md-block d-lg-none d-xl-none banner-medium">
-
-  </div>
-
-  <!-- large screens -->
-
-  <div class="d-none d-sm-none d-md-none d-lg-block d-xl-none banner-large">
-    
-
-  </div>
-
-  <!-- xl and up screens -->
-
-  <div class="d-none d-sm-none d-md-none d-lg-none d-xl-block banner-xl">
-    
-
-  </div>
+  <!-- xs -->     <div class="d-block d-xs-block d-sm-none d-md-none d-lg-none d-xl-none banner-xs"></div>
+  <!-- small -->  <div class="d-none d-sm-block d-md-none d-lg-none d-xl-none banner-small"></div>
+  <!-- medium --> <div class="d-none d-sm-none d-md-block d-lg-none d-xl-none banner-medium"></div>
+  <!-- large -->  <div class="d-none d-sm-none d-md-none d-lg-block d-xl-none banner-large"></div>
+  <!-- xl -->     <div class="d-none d-sm-none d-md-none d-lg-none d-xl-block banner-xl"></div>
